e35c8e5e71083556f5d6f0f384984cadc19e89b1
Michael Steil <mist64@mac.com>
2019-07-30 08:16:47 -0700

    mostly correct tables for shift, alt, ctrl
    all major characters are reachable, but not all cbm-based codes
    are reachable

--- ps2keyboard.s
+++ ps2keyboard.s
@@ -18,8 +18,8 @@ upper_byte = $fe
 
 MODIFIER_CAPS = 16
 MODIFIER_WIN = 8
-MODIFIER_ALT = 4
-MODIFIER_CTRL = 2
+MODIFIER_CTRL = 4
+MODIFIER_ALT = 2
 MODIFIER_SHIFT = 1
 
 .segment        "LOADADDR"
@@ -215,61 +215,6 @@ rcvsc5:	pha
 	pla ; lower byte into A
 	rts
 
-
-;****************************************
-;****************************************
-kbd_driver:
-	jsr receive_down_scancode_no_modifiers
-	beq drv_end
-
-	cpx #0
-	bne down_ext
-
-	tay
-	lda modifiers
-	beq use_tab_unshifted
-	lda #MODIFIER_SHIFT
-	bit modifiers
-	bne use_tab_shift
-	lda #MODIFIER_CTRL
-	bit modifiers
-	bne use_tab_ctrl
-	lda #MODIFIER_ALT
-	bit modifiers
-	bne use_tab_alt
-
-use_tab_unshifted:
-	tya
-	tax
-	lda tab_unshifted,x
-	jmp drvcont
-
-use_tab_shift:
-	tya
-	tax
-	lda tab_shift,x
-	jmp drvcont
-
-use_tab_ctrl:
-	tya
-	tax
-	lda tab_ctrl,x
-	jmp drvcont
-
-use_tab_alt:
-	tya
-	tax
-	lda tab_alt,x
-	jmp drvcont
-
-drvcont:
-	jsr add_to_buf
-
-down_ext:
-drv_end:
-
-	rts
-
 ;****************************************
 ; RECEIVE SCANCODE AFTER MODIFIERS
 ; * key down only
@@ -305,6 +250,7 @@ no_mod:	plp
 	bcs key_up
 no_key:	rts ; original Z is retained
 
+; XXX handle caps lock
 
 check_mod:
 	cmp #$11 ; left alt (0011) or right alt (E011)
@@ -334,6 +280,46 @@ md_sh:	lda #MODIFIER_SHIFT
 	rts
 
 ;****************************************
+;****************************************
+kbd_driver:
+	jsr receive_down_scancode_no_modifiers
+	beq drv_end
+
+	cpx #0
+	bne down_ext
+
+	cmp #$83
+	bne not_f7
+	lda #$02
+not_f7:
+
+	tay
+	ldx #0
+	lda modifiers
+find_bit:
+	lsr
+	bcs bit_found
+	inx
+	inx
+	cpx #6
+	bne find_bit
+
+bit_found:
+	lda tables,x
+	sta 2
+	lda tables + 1,x
+	sta 3
+	lda (2),y
+	beq drv_end
+	jsr add_to_buf
+
+down_ext:
+	; XXX decide extended characters
+drv_end:
+
+	rts
+
+;****************************************
 ; ADD CHAR TO KBD BUFFER
 ;****************************************
 add_to_buf:
@@ -349,18 +335,12 @@ add_to_buf:
 :	pla
 	rts
 
-PETSCII_F1 = $85
-PETSCII_F2 = $89
-PETSCII_F3 = $86
-PETSCII_F4 = $8A
-PETSCII_F5 = $87
-PETSCII_F6 = $8B
-PETSCII_F7 = $88
-PETSCII_F8 = $8C
+tables:
+	.word tab_shift, tab_alt, tab_ctrl, tab_unshifted
 
 tab_unshifted:
-	.byte $00,$00,$00,PETSCII_F5,PETSCII_F3,PETSCII_F1,PETSCII_F2,$00
-	.byte $00,$00,PETSCII_F8,PETSCII_F6,PETSCII_F4,$00,$7e,$00
+	.byte $00,$00,$88,$87,$86,$85,$89,$00
+	.byte $00,$00,$8C,$8B,$8A,$09,'_',$00
 	.byte $00,$00,$00,$00,$00,'Q','1',$00
 	.byte $00,$00,'Z','S','A','W','2',$00
 	.byte $00,'C','X','D','E','4','3',$00
@@ -369,66 +349,70 @@ tab_unshifted:
 	.byte $00,$00,'M','J','U','7','8',$00
 	.byte $00,',','K','I','O','0','9',$00
 	.byte $00,'.','/','L',';','P','-',$00
-	.byte $00,$00,$2a,$00,'[','=',$00,$00
+	.byte $00,$00,$27,$00,'[','=',$00,$00
 	.byte $00,$00,$0d,']',$00,'\',$00,$00
 	.byte $00,$00,$00,$00,$00,$00,$14,$00
+
 	.byte $00,'1',$00,'4','7',$00,$00,$00
 	.byte '0','.','2','5','6','8',$1b,$00
 	.byte $00,'+','3','-','*','9',$00,$00
 
 tab_shift:
-	.byte $00,$00,$00,PETSCII_F7,$00,$00,$00,$00
-	.byte $00,$00,$00,$00,$00,$00,$1b,$00
-	.byte $00,$00,$00,$00,$00,$71,$21,$00
-	.byte $00,$00,$7a,$73,$61,$77,$22,$00
-	.byte $00,$63,$78,$64,$65,$24,$23,$00
-	.byte $00,$20,$76,$66,$74,$72,$25,$00
-	.byte $00,$6e,$62,$68,$67,$79,$26,$00
-	.byte $00,$00,$6d,$6a,$75,$27,$28,$00
-	.byte $00,$3c,$6b,$69,$6f,$30,$29,$00
-	.byte $00,$3e,$3f,$6c,$3a,$70,$2d,$00
-	.byte $00,$00,$5f,$00,$7b,$2b,$00,$00
-	.byte $00,$00,$0d,$7d,$00,$7c,$00,$00
-	.byte $00,$00,$00,$00,$00,$00,$14,$00
-	.byte $00,$00,$00,$00,$00,$00,$00,$00
-	.byte $00,$14,$00,$00,$00,$00,$00,$00
-	.byte $00,$2b,$00,$2d,$00,$00,$00,$00
+	.byte $00,$00,$88,$87,$86,$85,$89,$00
+	.byte $00,$00,$8C,$8B,$8A,$18,$7e,$00
+	.byte $00,$00,$00,$00,$00,'Q'+$80,'!',$00,$00
+	.byte $00,'Z'+$80,'S'+$80,'A'+$80,'W'+$80,'@',$00
+	.byte $00,'C'+$80,'X'+$80,'D'+$80,'E'+$80,'$','#',$00
+	.byte $00,$a0,'V'+$80,'F'+$80,'T'+$80,'R'+$80,'%',$00
+	.byte $00,'N'+$80,'B'+$80,'H'+$80,'G'+$80,'Y'+$80,'^',$00
+	.byte $00,$00,'M'+$80,'J'+$80,'U'+$80,'&','*',$00
+	.byte $00,'<','K'+$80,'I'+$80,'O'+$80,')','(',$00
+	.byte $00,'>','?','L'+$80,':','P'+$80,$DD,$00
+	.byte $00,$00,'"',$00,'{','+',$00,$00
+	.byte $00,$00,$8d,'}',$00,$a9,$00,$00
+	.byte $00,$00,$00,$00,$00,$00,$94,$00
 
-tab_ctrl:
-	.byte $00,$00,$00,$00,$00,$00,$00,$ff
-	.byte $00,$00,$00,$00,$00,$00,$7e,$00
-	.byte $00,$00,$00,$00,$00,$11,$90,$00
-	.byte $00,$00,$1a,$13,$01,$17,$05,$00
-	.byte $00,$03,$18,$04,$05,$9f,$1c,$00
-	.byte $00,$20,$16,$06,$14,$12,$9c,$00
-	.byte $00,$0e,$02,$08,$07,$19,$1e,$00
-	.byte $00,$00,$0d,$0a,$15,$1f,$9e,$00
-	.byte $00,$2c,$0b,$09,$0f,$92,$12,$00
-	.byte $00,$2e,$2f,$0c,$3b,$10,$2d,$00
-	.byte $00,$00,$27,$00,$5b,$3d,$00,$00
-	.byte $00,$00,$0d,$5d,$00,$5c,$00,$00
-	.byte $00,$00,$00,$00,$00,$00,$14,$00
-	.byte $00,$00,$00,$00,$00,$00,$00,$00
-	.byte $00,$00,$00,$00,$00,$00,$00,$00
-	.byte $00,$2b,$00,$2d,$00,$00,$00,$00
+	.byte $00,'1',$00,'4','7',$00,$00,$00
+	.byte '0','.','2','5','6','8',$1b,$00
+	.byte $00,'+','3','-','*','9',$00,$00
 
 tab_alt:
-	.byte $00,$00,$00,$00,$00,$00,$00,$00
-	.byte $00,$00,$00,$00,$00,$00,$7e,$00
+	.byte $00,$00,$88,$8b,$8a,$89,$00,$00
+	.byte $00,$00,$00,$00,$00,$18,$7e,$00
 	.byte $00,$00,$00,$00,$00,$ab,$81,$00
 	.byte $00,$00,$ad,$ae,$b0,$b3,$95,$00
 	.byte $00,$bc,$bd,$ac,$b1,$97,$96,$00
-	.byte $00,$20,$be,$bb,$a3,$b2,$98,$00
-	.byte $00,$b6,$bf,$b4,$a5,$b7,$99,$00
+	.byte $00,$a0,$be,$bb,$a3,$b2,$98,$00
+	.byte $00,$aa,$bf,$b4,$a5,$b7,$99,$00
 	.byte $00,$00,$a7,$b5,$b8,$9a,$9b,$00
 	.byte $00,$3c,$a1,$a2,$b9,$30,$29,$00
-	.byte $00,$3e,$3f,$b6,$5d,$af,$7c,$00
-	.byte $00,$00,$27,$00,$40,$3d,$00,$00
-	.byte $00,$00,$0d,$5e,$00,$a8,$00,$00
-	.byte $00,$00,$00,$00,$00,$00,$14,$00
+	.byte $00,$3e,$3f,$b6,':',$af,$dc,$00
+	.byte $00,$00,'"',$00,$00,$3d,$00,$00
+	.byte $00,$00,$8d,$00,$00,$a8,$00,$00
+	.byte $00,$00,$00,$00,$00,$00,$94,$00
+
+	.byte $00,'1',$00,'4','7',$00,$00,$00
+	.byte '0','.','2','5','6','8',$1b,$00
+	.byte $00,'+','3','-','*','9',$00,$00
+
+tab_ctrl:
+	.byte $00,$00,$88,$87,$86,$85,$89,$00
+	.byte $00,$00,$8C,$8B,$8A,$18,$06,$00
+	.byte $00,$00,$00,$00,$00,$11,$90,$00
+	.byte $00,$00,$1a,$13,$01,$17,$05,$00
+	.byte $00,$03,$18,$04,$05,$9f,$1c,$00
+	.byte $00,$00,$16,$06,$14,$12,$9c,$00
+	.byte $00,$0e,$02,$08,$07,$19,$1e,$00
+	.byte $00,$00,$0d,$0a,$15,$1f,$9e,$00
+	.byte $00,$00,$0b,$09,$0f,$92,$12,$00
+	.byte $00,$00,$00,$0c,$1d,$10,$00,$00
+	.byte $00,$00,$00,$00,$00,$1f,$00,$00
+	.byte $00,$00,$00,$00,$00,$1c,$00,$00
 	.byte $00,$00,$00,$00,$00,$00,$00,$00
-	.byte $00,$00,$00,$00,$00,$00,$00,$00
-	.byte $00,$2b,$00,$2d,$00,$00,$00,$00
+
+	.byte $00,'1',$00,'4','7',$00,$00,$00
+	.byte '0','.','2','5','6','8',$1b,$00
+	.byte $00,'+','3','-','*','9',$00,$00
 
 ; References:
 ; * Microsoft: "Keyboard Scan Code Specification", Revision 1.3a — March 16, 2000
@@ -492,7 +476,7 @@ tab_alt:
 ; 4C            F0 4C               ;
 ; 4D            F0 4D               p
 ; 4E            F0 4E               -
-; 52            F0 52               "
+; 52            F0 52               '
 ; 54            F0 54               [
 ; 55            F0 55               =
 ; 58            F0 58               Caps Lock
